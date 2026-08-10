4ad324df0cdc8ea7ad66fcd6b9626eff4be58b3e
Abinas1 <beheraabinas246@gmail.com>
2026-08-10 22:32:13 +0530

    Feat: Added new theatre model

---
+++ models/theatre.model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+/* Define the scema of theatre resource to be stored in db */
+
+const theatreSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    description: String,
+    city :{
+        type:String,
+        required: true
+    },
+    pincode :{
+        type: Number,
+        required: true
+    },
+    address : String
+}, {timestamp : true}),
+
+const Theatre = mongoose.model('Theatre', theatreSchema);
+
+module.exports = Theatre;
