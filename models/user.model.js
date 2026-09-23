bd2156b344e8a8e77aec7de62cf9e9fb3a4809bf
Abinas1 <beheraabinas246@gmail.com>
2026-09-23 08:31:54 +0530

    Feat: Added user Schema

---
+++ models/user.model.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    email: {
+        type:String,
+        required: true,
+        unique:true,
+        lowercase:true,
+        match:[/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, 'Please fill a valid email'],
+        trim:true
+    },
+    password:{
+        type:String,
+        required: true,
+        minlength:6
+    },
+    userRole:{
+        type:String,
+        required:true,
+        default:"CUSTOMER"
+    },
+    userStatus: {
+        typre:String,
+        required: true,
+        default: "APPROVED"
+    }
+}, {timestamps:true});
+
+const User = mongoose.model('User',userSchema);
+module.exports = User;
